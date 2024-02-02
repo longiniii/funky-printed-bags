be24991f9d052d27a973511aad3a7970b65a93ee
longiniii <kitiashvili10@gmail.com>
2024-02-02 21:50:46 +0400

    change a few things

--- app.js
+++ app.js
@@ -1,3 +1,54 @@
-let about = document.getElementById('about')
+fetch('./data.json')
+.then(response => response.json())
+.then(data =>  products(data))
+let store = document.getElementById('store')
 let header = document.getElementById('header')
-about.style.marginTop = header.offsetHeight + 'px'
+store.style.marginTop = header.offsetHeight + 48 + 'px'
+let product;
+let productImage;
+let add;
+let discount;
+let topSeller;
+let products = (d) => {
+    d.forEach((item, index) => {
+        store.innerHTML +=
+        `<div class="s-product">
+            <div class="p-image-div">
+                <div class="id-top-seller"><p>Top Seller</p></div>
+                <img class="id-image" src="${item.images[0]}" alt="">
+                <div class="id-discount"><p>Discount!!</p></div>
+            </div>
+            <div class="p-text">
+                <p class="t-name">${item.name}</p>
+                <p class="t-cost">$${item.cost - 0.01}</p>
+                <button class="t-add">Add to Cart</button>
+            </div>
+        </div>`
+        product = document.querySelectorAll('.s-product')
+        productImage = document.querySelectorAll('.id-image')
+        add = document.querySelectorAll('.t-add')
+        discount = document.querySelectorAll('.id-discount')
+        topSeller = document.querySelectorAll('.id-top-seller')
+        if (!item.topSeller) {
+            topSeller[index].style.display = 'none'
+        }
+        if (!item.discount) {
+            discount[index].style.display = 'none'
+        }
+    });
+    product.forEach((element, index) => {
+        console.log(element)
+        element.addEventListener('mouseover', function() {
+            productImage[index].setAttribute('src',`${d[index].images_patterns[0]}`);
+        });
+        element.addEventListener('mouseout', function() {
+            productImage[index].setAttribute('src',`${d[index].images[0]}`);
+        });
+        element.addEventListener('mouseover', function() {
+            add[index].style.display = 'block'
+        });
+        element.addEventListener('mouseout', function() {
+            add[index].style.display = 'none'
+        });
+    });
+}
